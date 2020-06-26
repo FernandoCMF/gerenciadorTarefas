41ca06efa94869891c624ef604e3ece8c8cbc3dd
nando <fmfernando61@gmail.com>
2020-06-26 12:50:46 -0300

    Updates

---
+++ precache-manifest.bc6d341ce9deaa7edac6db22fffe0787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8cd4bbf2b3c06ae943f51f27f0e552",
+    "url": "/gerenciadorTarefas/index.html"
+  },
+  {
+    "revision": "148b7c13c763919cdd6c",
+    "url": "/gerenciadorTarefas/static/css/main.2154059e.chunk.css"
+  },
+  {
+    "revision": "dff4c5c20b63e6b6aad7",
+    "url": "/gerenciadorTarefas/static/js/2.1144ecfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gerenciadorTarefas/static/js/2.1144ecfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148b7c13c763919cdd6c",
+    "url": "/gerenciadorTarefas/static/js/main.e41f5454.chunk.js"
+  },
+  {
+    "revision": "305f609e3803d22b17f6",
+    "url": "/gerenciadorTarefas/static/js/runtime-main.54c1a64f.js"
+  }
+]);
